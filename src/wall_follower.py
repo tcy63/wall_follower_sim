fff565d0efa6ebcdc431cd6dfdeb6cd56098acb2
tcy63 <2000017814@stu.pku.edu.cn>
2023-02-27 17:48:05 -0500

    final submission

--- src/wall_follower.py
+++ src/wall_follower.py
@@ -5,10 +5,20 @@ import numpy as np
 import math
 
 import rospy
+import tf
+import tf2_ros
 from rospy.numpy_msg import numpy_msg
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
 from visualization_tools import *
+
+"""
+A helper function for visualizing a straight line
+"""
+def visualize(x, y, color=(0, 1, 0)):
+    publisher = rospy.Publisher('/wall', Marker, queue_size=1)
+    VisualizationTools.plot_line(x, y, publisher, color, frame="/laser")
+
 
 class WallFollower:
     # Import ROS parameters from the "params.yaml" file.
@@ -27,17 +37,16 @@ class WallFollower:
 
         # to accumulate errors here
         self.errors = [0]
-        self.kp = 10
+        self.kp = 2
         self.ki = 0
-        self.kd = 0.1
+        self.kd = 8
 
         # to publish messages of type AckermannDriveStamped to the /drive topic
         self.publisher = rospy.Publisher(self.DRIVE_TOPIC, AckermannDriveStamped, queue_size=10)
-
+        
         # to subscribe to LIDAR data of type LaserScan
         rospy.Subscriber(self.SCAN_TOPIC, numpy_msg(LaserScan), self.laser_callback)
 
-    # TODO:
     # Write your callback functions here.
     def laser_callback(self, data):
         # create the message that is going to be sent out
@@ -49,50 +58,49 @@ class WallFollower:
         angle_min = data.angle_min
         angle_max = data.angle_max
 
+        # compute an array of the angles 
+        angles = np.arange(angle_min, angle_max, angle_increment)   # [angle_min, angle_max)
+
         # get the parameters for the follower
         desired_distance = self.DESIRED_DISTANCE
         velocity = self.VELOCITY
         side = self.SIDE
 
-        # TODO: detect a wall in a laser scan
-        # slice the ranges to get [-\pi/2,0] if following the left wall and [0, \pi/2] if right wall
-        left_index = 0
-        right_index = len(distances)
-        wall_angle_min = -math.pi/2
-        wall_angle_max = math.pi/2
-
-        if side == 1:  # if the car is following the left wall
-            wall_angle_min = max(angle_min, wall_angle_min)
-            wall_angle_max = min(angle_max, 0)
-            if angle_min < -math.pi/2:
-                left_index = (-math.pi/2 - angle_min) / angle_increment
-            if angle_max >= 0:
-                right_index = (0 - angle_min) / angle_increment
-        else:   # the car is follwing the right wall
-            wall_angle_min = max(angle_min, 0)
-            wall_angle_max = min(angle_max, wall_angle_max)
-            if angle_min < 0:
-                left_index = (0 - angle_min) / angle_increment
-            if angle_max > math.pi/2:
-                right_index = (math.pi/2 - angle_min) / angle_increment
+        # compute the x-y coordinates of all points
+        points_x = np.multiply(distances, np.cos(angles))
+        points_y = np.multiply(distances, np.sin(angles))
         
-        wall_distances = distances[left_index:right_index+1]    # from wall_angle_min to wall_angle_max with an increment of angle_increment
+        # get the left/right and front regions
+        if side == -1:
+            wall_distances = distances[10:40]
+            wall_points_x = points_x[10:40]
+            wall_points_y = points_y[10:40]
+        else:
+            wall_distances = distances[60:90]
+            wall_points_x = points_x[60:90]
+            wall_points_y = points_y[60:90]
         
-        # take tha range data into (x,y) points in the car's local coordinate systems
-        wall_x = [wall_distances[i] * np.cos(wall_angle_min + i * angle_increment) for i in len(wall_distances)]
-        wall_y = [wall_distances[i] * np.sin(wall_angle_min + i * angle_increment) for i in len(wall_distances)]
-        
-        # fit a line to these points by least square linear regression
-        A = np.hstack(np.ones_like(wall_x), wall_x)
-        Y = wall_y
-        X = np.linalg.inv(np.transpose(A) @ A) @ np.transpose(A) @ Y
+        front_points_x = points_x[45:55]
+        front_points_y = points_y[45:55]
+        front_distances = distances[45:55]
 
-        # plot the estimated wall and calculate the car's distance from that wall
-        distance = X[0] / np.sqrt(np.power(X[1]) + 1)
+        # fit the line
+        k, b = np.polyfit(wall_points_x, wall_points_y, 1)
+        wall_points_y_fit = k * wall_points_x + b
 
-        # TODO: use PD or PID control
+        # visualize to check the fitting line
+        visualize(wall_points_x, wall_points_y_fit, (1, 0, 0))
 
-        error = desired_distance - distance
+        # compute the car's distance from the line
+        r = np.array([0, 0])
+        p1 = np.array([wall_points_x[0], wall_points_y_fit[0]])
+        p2 = np.array([wall_points_x[-1], wall_points_y_fit[-1]])
+
+        distance = np.abs(np.cross(p1 - r, p2 - p1) / np.linalg.norm(p2 - p1))
+
+        # PID control
+        error = distance - desired_distance
+        error *= side
 
         self.errors.append(error)
         proportional_error = error * self.kp
@@ -100,9 +108,23 @@ class WallFollower:
         derivative_error = (self.errors[-1] - self.errors[-2]) * self.kd
         angle = proportional_error + integral_error + derivative_error
 
+        # deal with special states
+        # special case 1: if it is going into a corner/front wall
+        if np.mean(front_distances) < 2.8 * desired_distance:
+            angle = -0.34 * side
+
+        # if side == 1:
+        #     if np.mean(wall_distances) > 3 * desired_distance:
+        #         angle = 0.34
+        # special case 2: if it is going to far from a wall
+        if np.mean(wall_distances) > 3 * desired_distance:
+            angle = 0.34 * side
+        
+        # publish the message
         drive_msg.header.stamp = rospy.Time.now()
-        drive_msg.drive.steering_angle = angle
+        drive_msg.drive.steering_angle = max(min(angle, 0.34), -0.34)
         drive_msg.drive.speed = velocity
+
         self.publisher.publish(drive_msg)
 
 
